29c1f4c036ca5f788951f0770aa43d2662af9c0e
Alexandre Dalamico <alexandre.dalamico@gmail.com>
2019-11-19 21:22:30 -0200

    pagina 70

---
+++ Linguagem C/Exercicio pagina 70/exercicio_1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int num1=10, 
+		num2=20, 
+		num3=30, 
+		num4=40, 
+		num5=50, 
+		num6=60;
+	char c1='c', 
+		c2='o', 
+		c3='e', 
+		c4='l', 
+		c5='h', 
+		c6='a';
+	
+	printf("%d, %d, %d, %d, %d, %d \n",num1,num2,num3,num4,num5,num6);
+	printf("O animal contido nas variaveis caracteres e a %c%c%c%c%c%c", c1,c2,c3,c4,c5,c6);
+	
+}
