5bd794ee045b1d154a0524f3c07f7d240cbdfe2d
Alexandre Dalamico <alexandre.dalamico@gmail.com>
2019-11-19 20:03:46 -0200

    Terceiro

--- Linguagem C/Segundo.cpp
+++ Linguagem C/Segundo.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	printf ("Teste %% %%\n"); //-> "Teste % %"
-	printf ("%f\n",40.345); //-> "40.345"
-	printf ("Um caractere %c e um inteiro %d\n",'D',120); // -> "Um caractere D e um inteiro 120"
-	printf ("%s e um exemplo\n","Este"); // -> "Este e um exemplo"
-	printf ("%s%d%%","Juros de ",10); // -> "Juros de 10%"
+	printf ("Teste %% %%\n"); /*-> "Teste % %"*/
+	printf ("%f\n",40.345); /*-> "40.345"*/
+	printf ("Um caractere %c e um inteiro %d\n",'D',120); // -> "Um caractere D e um inteiro 120"*/
+	printf ("%s e um exemplo\n","Este"); /* -> "Este e um exemplo"*/
+	printf ("%s%d%%","Juros de ",10); /* -> "Juros de 10%"*/
 	
-	return(0);
+	//return(0);
 }
---
+++ Linguagem C/comandoscanfcomstdlib.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(){
+	int valor, valor1, valor2;
+	
+	/*printf ("Informe um valor :");*/
+	printf ("Informe 3 valores :");
+	/*scanf ("%d", &valor);*/
+	scanf ("%d %d %d", &valor, &valor1, &valor2);
+	
+	/*printf ("O conteudo da variavel eh : %d\n", valor); */
+	printf ("O conteudo da variavel 1 eh : %d\n", valor);
+	printf ("O conteudo da variavel 2 eh : %d\n", valor1);
+	printf ("O conteudo da variavel 3 eh : %d\n", valor2);
+	
+	system("pause");
+	
+}
