d676d9cc49d01a38ec65c53c6e2847f79f6f5401
Alexandre Dalamico <alexandre.dalamico@gmail.com>
2019-11-20 17:38:50 -0300

    exercicio

---
+++ C aula 1/o_primeiro.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Um Primeiro Programa */
+int main () /* Função principal da aplicação */
+{
+	printf ("Ola! Eu estou vivo!\n"); /* Escreve na tela */
+	/*return(0);*/
+	system("pause");
+}
